1565bdadc94e767277acc52c073bfcaddcebcc9d
Masa0920 <60780550+Masa0920@users.noreply.github.com>
2021-07-03 09:03:30 +0900

    1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>bootstrapの練習</h1>
+      <p>bootstrapの練習をしてます</p>
+    </div>
+  );
+}
+
+export default App;
